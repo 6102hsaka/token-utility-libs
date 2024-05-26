c38ac78d0ea47c66b332ecb6b9d12d951eed44ff
Akash Sharma <akashsharma15105@gmail.com>
2024-05-26 23:36:15 +0530

    Added: Comments

--- lib/src/main/java/dev/sharma/akash/tokenutility/libs/util/blacklist/TokenBlacklistUtility.java
+++ lib/src/main/java/dev/sharma/akash/tokenutility/libs/util/blacklist/TokenBlacklistUtility.java
@@ -1,7 +1,26 @@
 package dev.sharma.akash.tokenutility.libs.util.blacklist;
 
+/**
+ * Utility interface for managing blacklisted tokens.
+ */
 public interface TokenBlacklistUtility {
+	/**
+	 * Blacklists a given token with a specified expiration time. This method adds
+	 * the token to the blacklist and ensures it is invalidated until the expiration
+	 * time.
+	 *
+	 * @param token          The token to be blacklisted.
+	 * @param expirationTime The expiration time in seconds since the epoch when the
+	 *                       token should be removed from the blacklist.
+	 */
 	void blacklistToken(String token, long expirationTime);
 
+	/**
+	 * Checks if a given token is blacklisted. This method verifies whether the
+	 * token is currently in the blacklist.
+	 *
+	 * @param token The token to check.
+	 * @return {@code true} if the token is blacklisted, {@code false} otherwise.
+	 */
 	boolean isTokenBlacklisted(String token);
 }
--- lib/src/main/java/dev/sharma/akash/tokenutility/libs/util/token/TokenUtility.java
+++ lib/src/main/java/dev/sharma/akash/tokenutility/libs/util/token/TokenUtility.java
@@ -2,10 +2,34 @@ package dev.sharma.akash.tokenutility.libs.util.token;
 
 import java.util.Map;
 
+/**
+ * Utility interface for managing Tokens.
+ */
 public interface TokenUtility {
+	/**
+	 * Generates a Token with the specified subject and claims.
+	 *
+	 * @param subject The subject for which the token is being generated, typically
+	 *                representing a user or entity.
+	 * @param claims  A map of additional claims to be included in the token
+	 *                payload.
+	 * @return A signed Token as a string.
+	 */
 	String generateToken(String subject, Map<String, Object> claims);
 
+	/**
+	 * Extracts the subject from a given Token.
+	 *
+	 * @param token The Token from which to extract the subject.
+	 * @return The subject contained within the Token.
+	 */
 	String getSubject(String token);
 
+	/**
+	 * Validates a given Token.
+	 *
+	 * @param token The Token to validate.
+	 * @return {@code true} if the token is valid, {@code false} otherwise.
+	 */
 	boolean isValidToken(String token);
 }
